d7dbb6ce2b83669b1a9b3647d44e44ac77c4ebb9
Rioyi <wilbert.angarita@gmail.com>
2017-03-14 15:53:48 -0400

    Se creo la funcion de constructor de matriz y pintar la matriz, se instalo colorize

---
+++ test.rb
@@ -0,0 +1,51 @@
+require 'colorize'
+
+#+Construye el tablero
+def tablero(m)
+  for i in 0..7
+    a = []
+    for j in 0..7
+      a << "| |"
+    end
+    m << a
+  end
+  #m[3][3] = "|O|"
+  #m[3][4] = "|X|"
+  #m[4][3] = "|X|"
+  #m[4][4] = "|O|"
+end
+
+#+Imprime la matriz
+def imprimir(m)
+	puts ""
+  puts "================================================".colorize(:color => :blue, :background => :white)
+  puts "================= MADRIGUERA ==================="
+  puts ""
+  puts "       0    1    2    3    4    5    6    7".colorize(:white)
+  for i in 0..m.length - 1
+    print "  #{i} ".colorize(:white)
+    for x in 0..m.length - 1
+      print "  #{m[i][x]}"
+    end
+    puts
+  end
+  puts ""
+  puts "=================== COSECHA ====================="
+  puts "=================================================".colorize(:color => :blue, :background => :white)
+  puts ""
+  return m
+end
+
+
+
+
+def main()
+	matriz = []
+	tablero(matriz)
+	imprimir(matriz)
+
+
+
+end
+
+main()
