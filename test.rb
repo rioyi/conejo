55c732c7e44f1a10ea34fc58f11938ab1140fda5
rioyi <wilbert.angarita@gmail.com>
2017-03-15 23:56:20 -0400

    test

---
+++ test.rb
@@ -0,0 +1,238 @@
+require 'colorize'
+
+=begin
+#+Construye el tablero
+def tablero_conejo(matriz_conejo,ancho)
+  for i in 0..14
+    a = []
+    for j in 0..ancho -1
+      a << "| |"
+    end
+    matriz_conejo << a
+  end
+end
+
+#SOLO PARA TEST
+def tablero_coyote(matriz_coyote,ancho)
+  for i in 0..14
+    a = []
+    for j in 0..ancho -1
+      a << "| |"
+    end
+    matriz_coyote << a
+  end
+end
+
+def insertar_conejos(matriz_conejo,numero_conejos)
+  if numero_conejos % 2 == 0
+    hembras = numero_conejos / 2
+    varones = numero_conejos / 2
+  else
+    hembras = numero_conejos / 2
+    varones = numero_conejos - hembras
+  end
+
+cuenta_hembra = 0
+cuenta_varones = 0
+indice = 0
+  loop do
+    indice = rand(matriz_conejo[0].length-1)
+      if (matriz_conejo[0][indice] == "| |" && hembras != cuenta_hembra && cuenta_hembra < matriz_conejo[0].length-1) 
+        matriz_conejo[0][indice] = "|♀|"
+        cuenta_hembra += 1
+        indice += 1
+      end
+      if (matriz_conejo[0][indice] == "| |" && varones != cuenta_varones && cuenta_varones < matriz_conejo[0].length-1)      
+        matriz_conejo[0][indice] = "|♂|"
+        cuenta_varones += 1
+        indice += 1
+      end
+    break if (cuenta_varones+cuenta_hembra) == numero_conejos
+  end
+end
+
+def insertar_coyotes(matriz_coyote,numero_conejos)
+  cuenta_coyote = 0
+  indice = 0
+
+  loop do
+    indice = rand(matriz_coyote[0].length-1)
+    if (matriz_coyote[14][indice] == "| |" && cuenta_coyote < numero_conejos)
+      matriz_coyote[14][indice] = "|*|"
+      cuenta_coyote += 1
+    end
+    break if cuenta_coyote == numero_conejos
+  end  
+end
+
+def tecla_mover
+  system("stty raw -echo")
+  t = STDIN.getc
+  system("stty -raw echo")
+  return t
+end
+
+def move
+
+
+end
+
+
+#Imprime la matriz
+def imprimir(matriz_conejo)
+  puts ""
+  puts "================================================".colorize(:color => :blue, :background => :white)
+  puts "================= MADRIGUERA ===================".colorize(:brown)
+  puts ""
+  puts " CONEJOS".colorize(:white)
+  puts ""
+  for i in 0..matriz_conejo.length - 1
+    #print "  #{i} ".colorize(:white)
+    for x in 0..matriz_conejo.length - 1
+      print "  #{matriz_conejo[i][x]}"
+    end
+    puts
+  end
+  puts ""
+  puts "=================== COSECHA =====================".colorize(:green)
+  puts "=================================================".colorize(:color => :blue, :background => :white)
+  puts ""
+  return matriz_conejo
+end
+
+#=begin
+def imprimir_coyote(matriz_coyote)
+  puts "COYOTE"
+  puts ""
+  for i in 0..matriz_coyote.length - 1
+    #print "  #{i} ".colorize(:white)
+    for x in 0..matriz_coyote.length - 1
+      print "  #{matriz_coyote[i][x]}"
+    end
+    puts
+  end
+  puts ""
+  puts ""
+  return matriz_coyote
+end
+
+#=end
+def menu_inicial(matriz_conejo,matriz_coyote)
+  ancho = 0
+  numero_conejos = 0
+    loop do
+      puts "ingrese el Numero de columnas (el numero max es 15)"
+      ancho = gets.chomp.to_i
+      break if ancho >= 5 && ancho < 15
+    end
+
+    puts "Ingrese el Numero de conejos"
+    numero_conejos = gets.chomp.to_i
+
+=begin
+    loop do
+      puts "Ingrese el Numero de conejos"
+      numero_conejos = gets.chomp.to_i
+      if numero_conejos < ancho && numero_conejos > 5     
+        insertar_conejos(matriz_conejo,numero_conejos)
+      end
+      break if numero_conejos >= 5 && numero_conejos <= ancho
+    end
+
+  #end
+
+
+  tablero_conejo(matriz_conejo,ancho)
+  tablero_coyote(matriz_coyote,ancho)
+  insertar_conejos(matriz_conejo,numero_conejos)
+  insertar_coyotes(matriz_coyote,numero_conejos)
+end
+
+#Funcion principal
+def main()
+  matriz_conejo = []
+  matriz_coyote = []
+  menu_inicial(matriz_conejo,matriz_coyote)
+
+  
+  imprimir(matriz_conejo)
+  imprimir_coyote(matriz_coyote)
+end
+
+main()
+
+=end
+
+matriz_conejo = []
+
+  for i in 0..5
+    a = []
+    for j in 0..7
+      a << "| |"
+    end
+    matriz_conejo << a
+  end
+
+  matriz_conejo[0][0] = "|c|"
+  matriz_conejo[0][1] = "|c|"
+
+def imprimir(matriz_conejo)
+  for i in 0..matriz_conejo.length - 1
+    #print "  #{i} ".colorize(:white)
+    for x in 0..matriz_conejo.length - 1
+      print "  #{matriz_conejo[i][x]}"
+    end
+    puts
+  end
+  
+end
+imprimir(matriz_conejo)
+
+
+def get_keypressed(matriz_conejo,jugada)
+  system("stty raw -echo")
+  t = STDIN.getc
+  system("stty -raw echo")
+  move_down(t,matriz_conejo,jugada)
+  return t
+end
+
+def move_down(t,matriz_conejo,jugada)
+  #jugada = 0
+  if t == "s"  && jugada < 5
+    a1 = []
+    a2 = []
+    for i in 0..matriz_conejo[jugada].length-1
+      a1 << matriz_conejo[jugada][i]
+      a2 << matriz_conejo[jugada + 1][i]
+    end
+      linea1 = a1
+      liena2 = a2
+
+      matriz_conejo[jugada] = liena2
+      matriz_conejo[jugada +1] = linea1
+  end
+end
+jugada = 0
+loop do
+  
+  print "Presione cualquier tecla (s para salir): "
+  t = get_keypressed(matriz_conejo,jugada)
+  if t == "s"
+    puts jugada += 1
+  end
+  puts
+  imprimir(matriz_conejo)
+  puts "estas #{t}"
+  
+  break if t == "p"
+end
+
+
+
+
+
+
+
+
+
